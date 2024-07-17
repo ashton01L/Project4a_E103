3593d409c97c6b9dd88b87037f6b633cf08c9ee5
ashton01L <ashtonamazing@gmail.com>
2024-07-17 00:08:33 -0500

    adjust to code style

--- fall_distance.py
+++ fall_distance.py
@@ -4,17 +4,16 @@
 # Description: Determine a distance an object falls in a specific time period due
 # to gravity, using a given formula: d = (1/2)gt^2.
 
-def fall_distance(t):
+def fall_distance(time):
     """
     Calculate the distance an object falls in a specific time period due to gravity,
     using a given formula.
-    :param t: t (float) = time in seconds over which the object has been falling.
-    :return: d (float) = distance in meters measuring how far the object has fallen
+    :param time: time (float) = time in seconds over which the object has been falling.
+    :return: dist (float) = distance in meters measuring how far the object has fallen
     """
-    g = 9.8  # acceleration due to gravity in m/s^2
-    d = (1 / 2) * g * t ** 2
-    return d
+    grav = 9.8  # acceleration due to gravity in m/s^2
+    dist = (1 / 2) * grav * time ** 2
+    return dist
 
-#d = fall_distance(4.5)
-#print(d)
-
+#dist = fall_distance(4.5)
+#print(dist)
